882f2390f525c969d18a9cdf94b80b02f5026ab5
lquint <83752971+lquint@users.noreply.github.com>
2023-02-06 21:20:33 +0100

    added tests for removeAddress

--- hardhat-project/test/Whitelist.js
+++ hardhat-project/test/Whitelist.js
@@ -44,7 +44,6 @@ describe("Adding addresses to whitelist", function(){
             await whitelist.connect(accounts[i]).addAddress()
             expect(await whitelist.whitelistedAddresses(accounts[i].address)).to.be.true
         }
-        
     })
 
     it("Should revert when an user tries to get whitelisted twice", async function(){
@@ -52,7 +51,6 @@ describe("Adding addresses to whitelist", function(){
 
         await whitelist.connect(accounts[1]).addAddress()
         await expect(whitelist.connect(accounts[1]).addAddress()).to.be.revertedWith("Sender has already been whitelisted")
-        
     })
 
     it("Should revert when user tries to get whitelisted when the list is full", async function(){
@@ -62,8 +60,38 @@ describe("Adding addresses to whitelist", function(){
             await whitelist.connect(accounts[i]).addAddress()
             expect(await whitelist.whitelistedAddresses(accounts[i].address)).to.be.true
         }
-
         await expect(whitelist.connect(accounts[whitelistCapacity+1]).addAddress()).to.be.revertedWith("Whitelist limit reached, additionnal members cannot be added")
-        
+    })
+})
+
+describe("Removing addresses from whitelist", function(){
+    it("Should remove whitelisted users in normal conditions", async function(){
+        const { whitelist, accounts, whitelistCapacity} = await loadFixture(deployWhitelistFixture);
+        for (let i=0;i<whitelistCapacity;i++){
+            await whitelist.connect(accounts[i]).addAddress()
+            expect(await whitelist.whitelistedAddresses(accounts[i].address)).to.be.true
+        }
+        expect(await whitelist.numberWhitelisted()).to.equal(whitelistCapacity)
+        for (let i=0;i<whitelistCapacity;i++){
+            await whitelist.connect(accounts[0]).removeAddress(accounts[i].address)
+            expect(await whitelist.whitelistedAddresses(accounts[i].address)).to.be.false
+            expect(await whitelist.numberWhitelisted()).to.equal(whitelistCapacity-(i+1))
+        }
+    })
+
+    it("Should revert if trying to remove from an empty whitelist", async function(){
+        const { whitelist, accounts} = await loadFixture(deployWhitelistFixture);
+        await expect(whitelist.connect(accounts[0]).removeAddress(accounts[0].address)).to.be.revertedWith("Whitelist is empty, there are no members to remove")
+    })
+
+    it("Should revert if trying to remove an address that is not on the whitelist", async function(){
+        const { whitelist, accounts} = await loadFixture(deployWhitelistFixture);
+        await whitelist.connect(accounts[0]).addAddress()
+        await expect(whitelist.connect(accounts[0]).removeAddress(accounts[1].address)).to.be.revertedWith("Target user is not whitelisted")
+    })
+
+    it("Should revert non-owner address tries to use removeAddress function", async function(){
+        const { whitelist, accounts} = await loadFixture(deployWhitelistFixture);
+        await expect(whitelist.connect(accounts[1]).removeAddress(accounts[1].address)).to.be.revertedWith("You are not the owner")
     })
 })
