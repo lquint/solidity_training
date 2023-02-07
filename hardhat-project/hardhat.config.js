597b635e07699c5fac7d2dbb1edf3380201c6208
lquint <83752971+lquint@users.noreply.github.com>
2023-02-07 21:02:22 +0100

    installed hardhat-gas-reporter

--- hardhat-project/hardhat.config.js
+++ hardhat-project/hardhat.config.js
@@ -1,6 +1,9 @@
 require("@nomicfoundation/hardhat-toolbox");
-
+require("hardhat-gas-reporter");
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.17",
+  gasReporter: {
+    enabled: true
+  }
 };
