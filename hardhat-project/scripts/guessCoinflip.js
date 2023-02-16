845ddee04fdb1ad0bfb910de2d6a1f448d284321
lquint <leo.quintrand@gmail.com>
2023-02-16 11:22:07 +0100

    redacted one coinflip guess

--- hardhat-project/scripts/guessCoinflip.js
+++ hardhat-project/scripts/guessCoinflip.js
@@ -1,6 +1,6 @@
 const hre = require("hardhat");
 
-async function main(){
+async function main() {
     const Coinflip = await hre.ethers.getContractFactory("CoinFlip");
     const coinflip = await Coinflip.deploy();
     await coinflip.deployed();
@@ -8,6 +8,19 @@ async function main(){
     console.log(
         `Coinflip deployed to ${coinflip.address}`
     );
+    const GuessCoinflip = await hre.ethers.getContractFactory("GuessCoinflip");
+    const guessCoinflip = await GuessCoinflip.deploy(coinflip.address);
+    await guessCoinflip.deployed();
+
+    console.log(
+        `GuessCoinflip deployed to ${guessCoinflip.address}`
+    );
+    let wins = await coinflip.consecutiveWins()
+    console.log("Current consecutive wins : ", wins)
+    console.log("Using exploit to guess a coinflip")
+    await guessCoinflip.attack()
+    wins = await coinflip.consecutiveWins()
+    console.log("Current consecutive wins : ", wins)
 }
 
 main().catch((error) => {
